bf840b0ba21b64222eb7d2192e9a5496e9a704e9
Isakova Mariya <bubawenka@gmail.com>
2023-10-25 11:45:48 +0300

    оптимизирует задание для расчета времени встречи

--- js/functions-dz5.js
+++ js/functions-dz5.js
@@ -1,18 +1,52 @@
-// Делу - время, проверка: входит ли свтреча в рамки рабочего дня
+// Делу - время, проверка: входит ли встреча в рамки рабочего дня
+/**
+ *
+ * @param {${number}:${number}} START_DAY
+ * @param {${number}:${number}} END_DAY
+ * @param {${number}:${number}} START_DATE
+ * @param {${number}} DURATION
+ *
+ * */
 
 const isDateInDay = (START_DAY, END_DAY, START_DATE, DURATION) => {
   const startDay = START_DAY.split(':');
-  const startDayMinuts = startDay[0] * 60 + startDay[1];
+  const startDayMinutes = startDay[0] * 60 + startDay[1];
   const endDay = END_DAY.split(':');
-  const endDayMinuts = endDay[0] * 60 + endDay[1];
+  const endDayMinutes = endDay[0] * 60 + endDay[1];
   const startDate = START_DATE.split(':');
-  const startDateMinuts = startDate[0] * 60 + startDate[1];
+  const startDateMinutes = startDate[0] * 60 + startDate[1];
 
-  if(startDateMinuts > startDayMinuts && (startDateMinuts + DURATION) < endDayMinuts){
+  if(startDateMinutes >= startDayMinutes && (startDateMinutes + DURATION) <= endDayMinutes){
     return true;
   }
 
   return false;
 };
 
-console.log(isDateInDay('8:00', '17:30', '08:00', 900));
+/**
+ * @param {${number}:${number}} startDay
+ * @param {${number}:${number}} endDay
+ * @param {${number}:${number}} startMeet
+ * @param {${number}} duration
+ * @param {${number}:${number}} time
+ */
+const MINUTES_IN_HOUR = 60;
+
+const getMinutes = (time) =>{
+  const [hour, minutes] = startDay.split(':').map(Number);
+  return hour * MINUTES_IN_HOUR + minutes;
+};
+
+const isMeetInDay = (startDay, endDay, startMeet, duration) => {
+  const startDayMinutes = getMinutes(startDay);
+  const endDayMinutes = getMinutes(endDay);
+  const startMeetMinutes = getMinutes(startMeet);
+
+  const isStartedInWorkShift = startDayMinutes >= startMeetMinutes;
+  const isEndedInWorkShift = (startMeetMinutes + duration) <= endDayMinutes;
+
+  return isStartedInWorkShift && isEndedInWorkShift;
+};
+
+isDateInDay('8:00', '17:30', '08:00', 900);
+isMeetInDay('8:00', '17:30', '08:00', 900);
