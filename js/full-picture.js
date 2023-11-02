d38aa3f0d6a9429fc6fb61243836fdf167aab251
Isakova Mariya <bubawenka@gmail.com>
2023-11-02 08:55:35 +0300

    привязывает к module8-task1

--- js/full-picture.js
+++ js/full-picture.js
@@ -5,10 +5,10 @@ const body = document.querySelector('body');
 // закрытие большой картинки
 
 const onDocumentKeydown = (evt) => {
-    if(evt.key === 'Escape') {
-      evt.preventDefault();
-      closeBigPicture();
-    }
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeBigPicture();
+  }
 };
 
 const closeBigPicture = () => {
@@ -18,24 +18,20 @@ const closeBigPicture = () => {
 };
 
 //открытие миниатюры в полноэкранном режиме
-/**
- *
- * @param {HTMLAnchorElement} photo
- */
 
 const openBigPicture = (photo) => {
-    body.classList.add('.modal-open');
-    bigPicture.classList.remove('hidden');
-    document.addEventListener('keydown', onDocumentKeydown);
+  body.classList.add('.modal-open');
+  bigPicture.classList.remove('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
 
-    const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
-    bigPictureImg.src = photo.url;
-    bigPictureImg.alt = photo.alt;
+  const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+  bigPictureImg.src = photo.url;
+  bigPictureImg.alt = photo.alt;
 
-    bigPicture.querySelector('.likes-count').textContent = photo.likes;
-    bigPicture.querySelector('.social__caption').textContent = photo.description;
+  bigPicture.querySelector('.likes-count').textContent = photo.likes;
+  bigPicture.querySelector('.social__caption').textContent = photo.description;
 
-    renderComments(photo.comments);
+  renderComments(photo.comments);
 };
 
 export { openBigPicture, closeBigPicture };
